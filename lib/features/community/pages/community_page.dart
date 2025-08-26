86a7f26bc51efee9cc718ab506cde9b161ae8710
azizbeeyla <azizbeeyla@gmail.com>
2025-08-26 16:36:01 +0500

    hammasi buldi

--- lib/features/community/pages/community_page.dart
+++ lib/features/community/pages/community_page.dart
@@ -1,81 +1,109 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lesson2_10/features/categories/widgets/Navigators.dart';
 import 'package:lesson2_10/features/community/widgets/recipe_community.dart';
 import 'package:provider/provider.dart';
 
 import '../../../core/utils/app_colors.dart';
 import '../managers/community_view_model.dart';
-import '../widgets/custom_tappbar.dart';
 
-
-class CommunityPage extends StatefulWidget {
+class CommunityPage extends StatelessWidget {
   const CommunityPage({super.key});
 
   @override
-  State<CommunityPage> createState() => _CommunityPageState();
-}
-
-class _CommunityPageState extends State<CommunityPage> {
-  int selectedIndex = 0;
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBody: true,
-      backgroundColor: AppColors.baige,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        extendBody: true,
+        backgroundColor: AppColors.baige,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: AppColors.baige,
 
-      appBar: CustomTabBar(
-        selectedIndex: selectedIndex,
-        onTabSelected: (index) {
-          setState(() {
-            selectedIndex = index;
-          });
-        },
+          title: Text("Community",style: TextStyle(color: AppColors.mainpink,fontSize: 20.sp),),
+          bottom: TabBar(
+            indicator: BoxDecoration(
+              color: AppColors.mainpink,
+              borderRadius: BorderRadius.circular(18.r),
+            ),
+            indicatorColor: Colors.transparent,
+            labelColor: AppColors.whiteText,
+            unselectedLabelColor: AppColors.mainpink,
+            dividerColor: Colors.transparent,
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorPadding: EdgeInsets.symmetric(
+              horizontal: 20.w,
+              vertical: 10.h,
+            ),
+            tabs: [
+              Tab(text: "Top Recipes"),
+              Tab(text: "Newest"),
+              Tab(text: "Oldest"),
+            ],
+          ),
+        ),
+        body: ChangeNotifierProvider(
+          create: (context) => CommunityViewModel(communityRepo: context.read()),
+          builder: (context, chil) => TabBarView(
+            children: [
+              Consumer<CommunityViewModel>(
+                builder: (context, vm, child) => vm.isLoadingTop
+                    ? Center(
+                  child: CircularProgressIndicator(),
+                )
+                    : SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 126.h),
+                    child: RecipeCommunity(
+                      vm: vm.communityTop,
+                      viewModel:vm.formatTimeAgo,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(36.w, 22.h, 36.w, 0),
+                child: Consumer<CommunityViewModel>(
+                  builder: (context, vm, child) => vm.isLoadingNew
+                      ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                      : SingleChildScrollView(
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: 126.h),
+                      child: RecipeCommunity(
+                        vm: vm.communityNew,
+                        viewModel: vm.formatTimeAgo,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(36.w, 22.h, 36.w, 0),
+                child: Consumer<CommunityViewModel>(
+                  builder: (context, vm, child) => vm.isLoadingOld
+                      ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                      : SingleChildScrollView(
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: 126.h),
+                      child: RecipeCommunity(
+                        vm: vm.communityOld,
+                        viewModel:vm.formatTimeAgo,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+bottomNavigationBar: Navigations(),
       ),
-
-      body: ChangeNotifierProvider(
-        create: (context) =>
-            CommunityViewModel(communityRepo: context.read()),
-        builder: (context, child) {
-          return Consumer<CommunityViewModel>(
-            builder: (context, vm, _) {
-              if (selectedIndex == 0) {
-                // Top Recipes
-                return vm.isLoadingTop
-                    ? const Center(child: CircularProgressIndicator())
-                    : SingleChildScrollView(
-                  child: RecipeCommunity(
-                    vm: vm.communityTop,
-                    viewModel: (_){},
-                  ),
-                );
-              } else if (selectedIndex == 1) {
-                // Newest
-                return vm.isLoadingNew
-                    ? const Center(child: CircularProgressIndicator())
-                    : SingleChildScrollView(
-                  child: RecipeCommunity(
-                    vm: vm.communityNew,
-                    viewModel: (_){},
-                  ),
-                );
-              } else {
-
-                return vm.isLoadingOld
-                    ? const Center(child: CircularProgressIndicator())
-                    : SingleChildScrollView(
-                  child: RecipeCommunity(
-                    vm: vm.communityOld,
-                    viewModel: (_){},
-                  ),
-                );
-              }
-            },
-          );
-        },
-      ),
-
-
     );
   }
 }
